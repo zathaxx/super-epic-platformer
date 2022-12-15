f9f23f5a364c8a2dabf5c1aa60c30971551eff85
Josias <me@josias.dev>
2022-12-15 14:48:02 -0800

    Enlarge game
    This allows more fine-grained control over locations and velocity.
    
    We don't increase jump velocity here because we want to ensure that
    the platform bug is easily reproducible for now.
    
    Co-authored-by: zathaxx <zathaxx@gmail.com>

--- src/level.rs
+++ src/level.rs
@@ -4,8 +4,8 @@ use raylib::prelude::*;
 fn default_character(grid_x: i32, grid_y: i32) -> Character {
     Character {
         hitbox: Hitbox {
-            w: 30,
-            h: 30,
+            w: 60,
+            h: 60,
             x: grid_x + 1000,
             y: grid_y,
         },
@@ -49,19 +49,19 @@ pub fn level_one() -> Level {
             Platform {
                 hitbox: Hitbox {
                     x: grid_x + 950,
-                    y: grid_y - 110,
-                    w: 100,
-                    h: 20,
+                    y: grid_y - 220,
+                    w: 200,
+                    h: 40,
                 },
                 surface: default.clone(),
                 color: Color::BLACK,
             },
             Platform {
                 hitbox: Hitbox {
-                    x: grid_x + 950,
-                    y: grid_y - 210,
-                    w: 100,
-                    h: 20,
+                    x: grid_x + 850,
+                    y: grid_y - 420,
+                    w: 200,
+                    h: 40,
                 },
                 surface: transparent.clone(),
                 color: Color::YELLOW,
@@ -69,19 +69,19 @@ pub fn level_one() -> Level {
             Platform {
                 hitbox: Hitbox {
                     x: grid_x + 1150,
-                    y: grid_y - 210,
-                    w: 100,
-                    h: 20,
+                    y: grid_y - 420,
+                    w: 200,
+                    h: 40,
                 },
                 surface: default.clone(),
                 color: Color::BLACK,
             },
             Platform {
                 hitbox: Hitbox {
-                    x: grid_x + 1350,
-                    y: grid_y - 210,
-                    w: 100,
-                    h: 20,
+                    x: grid_x + 1550,
+                    y: grid_y - 420,
+                    w: 200,
+                    h: 40,
                 },
                 surface: bouncy.clone(),
                 color: Color::GREEN,
